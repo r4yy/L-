e89db00a1b2f6ace916f4addfe10a11db644d4c6
r4yy <l0st@live.de>
2015-02-11 00:11:25 +0100

    Release

---
+++ PassAdder/PassAdder/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PassAdder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dlgOffnen = new System.Windows.Forms.OpenFileDialog();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbAdd = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dlgOffnen
+            // 
+            this.dlgOffnen.Filter = "Textdateien(*.txt)|*txt|Alle Dateien (*.*)|*.*";
+            this.dlgOffnen.InitialDirectory = "parent";
+            this.dlgOffnen.Title = "Test";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCreate.AutoSize = true;
+            this.btnCreate.Location = new System.Drawing.Point(28, 147);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "OK";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(135, 147);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password extension:";
+            // 
+            // txbAdd
+            // 
+            this.txbAdd.Location = new System.Drawing.Point(135, 27);
+            this.txbAdd.Name = "txbAdd";
+            this.txbAdd.Size = new System.Drawing.Size(75, 20);
+            this.txbAdd.TabIndex = 4;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(135, 53);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Open..";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCreate;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(236, 262);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txbAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "Form1";
+            this.Text = "PassAdder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog dlgOffnen;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbAdd;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ PassAdder/PassAdder/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PassAdder
+{
+    public partial class Form1 : Form
+    {
+        private StreamReader dateiReader;
+        private StreamWriter dateiWriter = new StreamWriter("output.txt");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            DialogResult dlgResult = dlgOffnen.ShowDialog();
+            if (dlgResult != DialogResult.OK)
+                return;
+
+            string dateiname = dlgOffnen.FileName;
+            dateiReader = new StreamReader(dateiname);
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                while (dateiReader.Peek() != -1)
+                {
+                    string zeile = dateiReader.ReadLine();
+                    zeile = zeile + ':' + zeile + txbAdd.Text;
+
+                    dateiWriter.WriteLine(zeile);
+                    //dateiWriter.Dispose();
+                }
+
+                MessageBox.Show(text: @"Done");
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show(text: @"Failed");
+            }
+            
+            dateiWriter.Close();
+            dateiReader.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
